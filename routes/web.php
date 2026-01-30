033e42379488f38ffe563be36ab37fbd499f8617
samd <sershdiaz@hotmail.com>
2026-01-30 14:59:36 -0600

    Error de deploy

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
 
